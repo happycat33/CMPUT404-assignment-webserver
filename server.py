61434ccf09a98ebeff212cbd95860dc80debbe03
Kimberly Tran <ktran5@cs.ualberta.ca>
2022-09-27 12:51:04 -0600

    revert changes that cause issue and added deep.png

--- server.py
+++ server.py
@@ -44,16 +44,15 @@ class MyWebServer(socketserver.BaseRequestHandler):
         request = (str(self.data).replace("b'","")).split("\\r\\n")[0]
         request_list = request.split(" ")
         request_path = request_list[1]
-        real_path = os.path.realpath(request_path)
-        full_path = self.base_path + real_path
-        print(full_path)
+        full_path = self.base_path + request_path
         if request_list[0] == "GET":
             if os.path.isdir(full_path) and full_path[-1] != '/':
-                header = 'HTTP/1.1 301 Moved permanently\r\ncontent-type: text/html\r\nlocation: http://127.0.0.1:8080%s/\r\n' % real_path
+                header = 'HTTP/1.1 301 Moved permanently\r\ncontent-type: text/html\r\nlocation: http://127.0.0.1:8080%s/\r\n' % request_path
                 self.response = header
 
             else:
                 if os.path.isdir(full_path):
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                     html_file = None
                     header = 'HTTP/1.1 200 OK\r\ncontent-type: text/html\r\n\r\n'
 
@@ -68,7 +67,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
                 else:
                     file_name = (full_path.split('/'))[-1]
-                    if os.path.exists(full_path):
+                    if os.path.isfile(full_path):
                         if ".html" in file_name:
                             header = 'HTTP/1.1 200 OK\r\ncontent-type: text/html\r\n\r\n'
                             
